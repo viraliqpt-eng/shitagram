015a7c5529f29b3bbb5f8e3b4814cda419723416
viraliqpt-eng <viraliqpt@gmail.com>
2026-06-06 23:30:49 +0100

    Add files via upload

--- livekit-config.js
+++ livekit-config.js
@@ -1,6 +1,4 @@
 // SHITAGRAM™ LiveKit Config
-// Este ficheiro pode ficar público no site.
-// NÃO colocar API Secret aqui.
 
 window.SHITAGRAM_LIVEKIT_URL = "wss://shitagram-arena-9s2vmtau.livekit.cloud";
 window.SHITAGRAM_LIVEKIT_TOKEN_ENDPOINT = "https://dcupiiiifnfffdsyyccyac.supabase.co/functions/v1/livekit-token";
