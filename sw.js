6f7aba7b2dad36de08ea499b45dd9b3ec3a3b810
viraliqpt-eng <viraliqpt@gmail.com>
2026-06-06 18:04:25 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'shitagram-arena-v18';
+
+const ASSETS = [
+  './arena.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(() => null)
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copy))
+          .catch(() => null);
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request)
+          .then(cached => cached || caches.match('./arena.html'))
+      )
+  );
+});
