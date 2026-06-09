cfd061f34ed6b094cf1fdb4fbfa875cd15f58d44
viraliqpt-eng <viraliqpt@gmail.com>
2026-06-09 23:57:59 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-// SHITAGRAM™ Supabase Config
-
-window.SHITAGRAM_SUPABASE_URL = "https://dcupiiiifnfffdsyyccyac.supabase.co";
-window.SHITAGRAM_SUPABASE_ANON_KEY = "sb_publishable_oeiwoIvG0wUvSdHLL2_OkQ_RawWzjP9";
